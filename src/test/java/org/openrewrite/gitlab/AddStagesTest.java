98aa22ace9eaa9697cc8da1fdfe0f78d64518c88
Steven Tompkins <steven.tompkins.jr@gmail.com>
2024-12-03 15:05:22 -0600

    Add `AddStages` recipe (#14)
    * Added recipe to update stages #13
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Also show behavior with acceptTheirs as true
    
    * Mark acceptTheirs as nullable
    
    ---------
    
    Co-authored-by: Steven Tompkins <stompkins@shelterinsurance.com>
    Co-authored-by: Tim te Beek <timtebeek@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Tim te Beek <tim@moderne.io>

---
+++ src/test/java/org/openrewrite/gitlab/AddStagesTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.gitlab;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import java.util.List;
+
+import static org.openrewrite.yaml.Assertions.yaml;
+
+class AddStagesTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new AddStages(List.of("build", "test", "deploy"), false));
+    }
+
+    @DocumentExample
+    @Test
+    void addNew() {
+        //language=yaml
+        rewriteRun(
+          yaml(
+            """
+              """,
+            """
+              stages:
+                - build
+                - test
+                - deploy
+              """,
+            source -> source.path(".gitlab-ci.yml")
+          )
+        );
+    }
+
+    @Test
+    void replaceExisting() {
+        //language=yaml
+        rewriteRun(
+          yaml(
+            """
+              stages:
+                - build
+              """,
+            """
+              stages:
+                - build
+                - test
+                - deploy
+              """,
+            source -> source.path(".gitlab-ci.yml")
+          )
+        );
+    }
+
+    @Test
+    void retainExisting() {
+        //language=yaml
+        rewriteRun(
+          spec ->
+            spec.recipe(new AddStages(List.of("build", "test", "deploy"), true)),
+          yaml(
+            """
+              stages:
+                - build
+              """,
+            source -> source.path(".gitlab-ci.yml")
+          )
+        );
+    }
+}
