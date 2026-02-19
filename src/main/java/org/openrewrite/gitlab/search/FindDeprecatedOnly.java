d4f74b023f786473d85cf17ea085cbe816c81dec
Steven Tompkins <steven@moderne.io>
2026-02-19 15:57:18 -0600

    Add search recipes for deprecated `only` and `except` keywords
    Closes #31

---
+++ src/main/java/org/openrewrite/gitlab/search/FindDeprecatedOnly.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.gitlab.search;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.Recipe;
+import org.openrewrite.yaml.search.FindKey;
+
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+
+@EqualsAndHashCode(callSuper = false)
+@Value
+public class FindDeprecatedOnly extends Recipe {
+
+    String displayName = "Find deprecated `only` keyword";
+
+    String description = "Find usages of the deprecated `only` keyword in `.gitlab-ci.yml`. " +
+            "The `only` keyword is deprecated in favor of `rules`.";
+
+    @Override
+    public List<Recipe> getRecipeList() {
+        return singletonList(new FindKey("$..only"));
+    }
+}
